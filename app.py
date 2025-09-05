7837fbfb7a93f3b9c918c6f4dd84a4c396d7c381
lhernandezs <lhernandez331@gmail.com>
2025-09-05 17:15:05 -0500

    5C 5 sep 2025

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 import os
 import pandas as pd
 
-from flask      import ( request, session, Flask, render_template, redirect, url_for, jsonify)
+from flask   import ( request, session, Flask, render_template, redirect, url_for, jsonify)
 
-from config                     import Config
-from cargadorDatos              import CargadorDatos
-from procesadorJuicios          import ProcesadorJuicios
-from entradaHelper              import getDataFrame
-
-from correo                     import Correo
-
-from config                     import TEMPLATES_FOLDER
+from config               import Config, TEMPLATES_FOLDER
+from cargadorDatos        import CargadorDatos
+from procesadorJuicios    import ProcesadorJuicios
+from entradaHelper        import getDataFrame
+from correo               import Correo
 
 app = Flask(__name__, template_folder=TEMPLATES_FOLDER)
 app.config.from_object(Config)
@@ -23,7 +20,7 @@ ALLOWED_EXTENSIONS  = app.config["ALLOWED_EXTENSIONS"]
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 os.makedirs(UPLOAD_FOLDER_DATA, exist_ok=True)
 
-def allowed_file(filename):
+def allowed_file(filename) -> bool:
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def delete_file_disk(ficha):
@@ -117,17 +114,11 @@ def delete_file(ficha):
 
 @app.route("/datos/<ficha>", methods=["POST"])
 def view_datos(ficha):
-    columnas = ["tipo", "documento", "nombres", "apellidos", "estado", "aprobado", "porEvaluar", "noAprobado", "enTramite", "activo", \
-                          "IND", "BIL", "CIE", "COM", "CUL", "DER", "EMP", "ETI", "INV", "MAT", "SST", "TIC", "PRO", "TEC", "color" ]
     try:
-        df_datos: pd.DataFrame = getDataFrame(UPLOAD_FOLDER, f"{ficha}.xlsx", "Datos", columnas)
+        df_datos: pd.DataFrame = getDataFrame(UPLOAD_FOLDER, f"{ficha}.xlsx", "datos")
         for col in df_datos.columns:
             df_datos[col] = df_datos[col].apply(
             lambda x: '' if x == 0 or pd.isna(x) or x == '' else (int(x) if isinstance(x, (int, float)) and not isinstance(x, bool) and float(x).is_integer() else x))
-
-
-        print("paso por aqui")
-
     except Exception as e:
         if 'error' not in session:
             session['error'] = f"Error: no fue posible abrir la hoja 'Datos' del archivo {e}"
@@ -135,28 +126,21 @@ def view_datos(ficha):
             session['error'].append(f"Error: no fue posible abrir la hoja 'Datos' del archivo {e}")
     return render_template("datos.html", dataFrame=df_datos, variables = session['fichas'][ficha], ficha= ficha)
 
-# Ruta para recibir los datos del formulario y enviar el correo
 @app.route('/mail', methods=["POST"])
 def mail():
     form_data = request.form.to_dict(flat=True)
     variables = form_data['variables']
     ficha = form_data['ficha']
     body = form_data['body']
-
     return render_template("correo.html", variables = variables, ficha = ficha, body = body)    
 
 @app.route('/send_mail', methods=["POST"])
 def send_mail():
-
     form_data = request.form.to_dict(flat=True)
     datos_correo = form_data['datos_correo']
     print(datos_correo)
-
     destination_username, destination_domain = "lhernandezs@sena.edu.co".split('@')
-
-    # correo = Correo('JUICI', destination_username, destination_domain, destination_username, datosCorreo)   # destino correo Leonardo            
-    correo = Correo('NONE', destination_username, destination_domain, destination_username, datos_correo)   # destino correo Leonardo            
-
+    correo = Correo(destination_username, destination_domain, destination_username, datos_correo)
     try:
         correo.send_email()
         return jsonify({'message': 'Correo enviado exitosamente!'})
